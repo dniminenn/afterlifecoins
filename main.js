1968a1d6ff731f9ba0c9d441abe1fd08562b52ce
dniminenn <85250163+dniminenn@users.noreply.github.com>
2023-02-28 00:27:28 -0400

    works as intended

--- main.js
+++ main.js
@@ -1,76 +1,75 @@
-// Connect to Metamask
-async function connect() {
-  if (typeof window.ethereum !== 'undefined') {
-    try {
-      // Connect to Fantom Opera network
-      await window.ethereum.request({
-        method: 'wallet_addEthereumChain',
-        params: [
-          {
-            chainId: '0xfa',
-            chainName: 'Fantom Opera',
-            nativeCurrency: {
-              name: 'FTM',
-              symbol: 'FTM',
-              decimals: 18
-            },
-            rpcUrls: ['https://rpcapi.fantom.network'],
-            blockExplorerUrls: ['https://ftmscan.com/']
-          }
-        ]
-      });
-      const accounts = await web3.eth.getAccounts();
-      const from = accounts[0];
-      const contractAddress = '0x8d9Ab6Aa28720a28D75CA157F7E16E9109520bAd';
-      const afterlifeAbi = await fetch('afterlife.abi').then(response => response.json());
-      const tokenContract = new web3.eth.Contract(afterlifeAbi, contractAddress);
-      const balances = await tokenContract.methods.balanceOfBatch([from], [98, 100, 110]).call();
-      const balancesElement = document.getElementById('balances');
-      balancesElement.innerHTML = `Token balance for variety 98: ${balances[0]}<br>Token balance for variety 100: ${balances[1]}<br>Token balance for variety 110: ${balances[2]}`;
-      await window.ethereum.enable();
-      return true;
-    } catch (error) {
-      return false;
-    }
-  }
-  return false;
+const tokenContractAddress = '0x8d9Ab6Aa28720a28D75CA157F7E16E9109520bAd';
+
+// Instantiate the contract object
+const afterlife = web3.eth.contract(afterlifeAbi);
+const tokenContract = afterlife.at(tokenContractAddress);
+
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
 }
 
-// Send ERC1155 token
-async function sendToken() {
-  const receiver = '0x3cc35873a61D925Ac46984f8C4F85d8fa6A892eF';
-  const amount = document.getElementById('amount').value;
-  const tokenId = document.querySelector('input[name="tokenid"]:checked').value;
-  const contractAddress = '0x8d9Ab6Aa28720a28D75CA157F7E16E9109520bAd';
-  const afterlifeAbi = await fetch('afterlife.abi').then(response => response.json());
-  const tokenContract = new web3.eth.Contract(afterlifeAbi, contractAddress);
-  const accounts = await web3.eth.getAccounts();
-  const from = accounts[0];
-  const options = { from: from };
-  const messageElement = document.getElementById('message');
-  try {
-    const result = await tokenContract.methods.safeTransferFrom(from, receiver, tokenId, amount, '0x00').send(options);
-    console.log('Token sent:', result);
-    const successMessage = `Token transfer was successful: ${amount} tokens of variety ${tokenId} were sent to ${receiver}`;
-    messageElement.innerHTML = `<div class="alert alert-success" role="alert">${successMessage}</div>`;
-    const balances = await tokenContract.methods.balanceOfBatch([from], [98, 100, 110]).call();
-    const balancesElement = document.getElementById('balances');
-    balancesElement.innerHTML = `Token balance for variety 98: ${balances[0]}<br>Token balance for variety 100: ${balances[1]}<br>Token balance for variety 110: ${balances[2]}`;
-  } catch (error) {
-    console.error('Error sending token:', error);
-    const errorMessage = 'An error occurred while sending the token. Please try again later.';
-    messageElement.innerHTML = `<div class="alert alert-danger" role="alert">${errorMessage}</div>`;
+window.addEventListener('load', async () => {
+  // Wait for MetaMask to load
+  if (typeof window.ethereum !== 'undefined') {
+    // Add event listener for connect button
+    document.getElementById('connectButton').addEventListener('click', async () => {
+      try {
+        // Check if the user is connected to the correct network
+        const chainId = await ethereum.request({ method: 'eth_chainId' });
+        if (chainId !== '0xfa') {
+          document.getElementById('message').innerHTML = '<div class="alert alert-danger" role="alert">Please connect to the Fantom Opera network to use this dApp.</div>';
+          return;
+        } else {
+	  document.getElementById('connectButton').innerHTML = 'Show total balance';
+	}
+        // Request account access
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        // Set up web3 object
+        web3 = new Web3(window.ethereum);
+        initApp();
+      } catch (error) {
+        console.error(error);
+        document.getElementById('message').innerHTML = '<div class="alert alert-danger" role="alert">Failed to connect to MetaMask.</div>';
+      }
+    });
+  } else {
+    // If MetaMask is not installed or not running, display an error
+    document.getElementById('message').innerHTML = '<div class="alert alert-danger" role="alert">Please install and connect to MetaMask to use this dApp.</div>';
   }
-}
+});
 
 // Handle form submission
-const form = document.getElementById('sendTokenForm');
-form.addEventListener('submit', async function(event) {
+document.getElementById('sendTokenForm').addEventListener('submit', async (event) => {
   event.preventDefault();
-  const isConnected = await connect();
-  if (!isConnected) {
-    alert('Please install and connect to Metamask to send tokens');
-    return;
-  }
-  await sendToken();
+  // Get the selected token ID
+  const selectedTokenId = document.querySelector('input[name="tokenid"]:checked').value;
+  // Get the recipient address
+  const recipientAddress = document.querySelector('input[name="rcvaddress"]').value;
+  // Get the amount
+  const amount = event.target.elements.amount.value;
+  // Call the transfer function of the ERC1155 token contract
+  tokenContract.safeTransferFrom(web3.eth.defaultAccount, recipientAddress, selectedTokenId, amount, [], function(err, res) {
+      if(!err)
+         document.getElementById('message').innerHTML = '<div class="alert alert-success" role="alert">Transfer successful!</div>';
+      else
+         document.getElementById('message').innerHTML = '<div class="alert alert-danger" role="alert">Transfer failed.</div>';
+  });
 });
+
+// Initialize the app
+async function initApp() {
+   // Get the balances of the three token types
+  tokenContract.balanceOf(web3.eth.defaultAccount, '98', function (err, res){document.getElementById('pennycount').innerHTML = res.c[0];});
+  tokenContract.balanceOf(web3.eth.defaultAccount, '100', function (err, res){document.getElementById('shillcount').innerHTML = res.c[0];});
+  tokenContract.balanceOf(web3.eth.defaultAccount, '110', function (err, res){document.getElementById('koincount').innerHTML = res.c[0];});
+  var pennies = parseInt(document.getElementById('pennycount').textContent, 10);
+  var shills = parseInt(document.getElementById('shillcount').textContent, 10)*2;
+  var koins = parseInt(document.getElementById('koincount').textContent, 10)*20;
+  var total = pennies + shills + koins;
+  if(!isNaN(total)) { document.getElementById('totalcount').innerHTML = total; }
+}
+
